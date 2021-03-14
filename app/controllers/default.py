3264c33349c0741dc3eee1e06fd09d46644fd674
franciscofolli <franciscoabel2001@gmail.com>
2021-03-14 09:49:59 -0300

    subindo manipulação de usuários

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,12 +1,13 @@
 from flask import render_template
 from app import app
 
+from app.models.forms import LoginForm
+
 # decorator utilizando para definir o nome das rotas
 # cada função pode ter mais de um nome de rota, conforme abaixo
-@app.route('/<user>')
-@app.route('/', defaults={'user':None})
-def index(user):
-    return render_template('index.html', user=user)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 # é possível realizar a passagem de parametros nas rotas, conforme abaixo
 @app.route('/test') # duas rotas para nao obrigar preenchimento de nome na rota
@@ -18,6 +19,13 @@ def teste(name=None): # mesmo nome do parametro
     else: 
         return 'Olá, Usuário!'
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    else: 
+        print(form.errors)
+    return render_template('login.html',
+                            form=form)
--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 DEBUG = True
+WTF_CSRF_ENABLED = False
+WTF_CSRF_SECRET_KEY = 'python_social'
