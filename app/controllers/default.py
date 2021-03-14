4404e8d73f4bc2ecb44d79bc77ac18a39f2f15b1
franciscofolli <franciscoabel2001@gmail.com>
2021-03-14 07:53:57 -0300

    subindo pacote de templates, com template base e conceito de integração python x html

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,5 +1,23 @@
+from flask import render_template
 from app import app
 
-@app.route("/")
-def index():
-    return "Ola Mundo!"
+# decorator utilizando para definir o nome das rotas
+# cada função pode ter mais de um nome de rota, conforme abaixo
+@app.route('/<user>')
+@app.route('/', defaults={'user':None})
+def index(user):
+    return render_template('index.html', user=user)
+
+# é possível realizar a passagem de parametros nas rotas, conforme abaixo
+@app.route('/test') # duas rotas para nao obrigar preenchimento de nome na rota
+# <int:name> com o int: serve para converter o valor passado para o tipo necessário para sua utilização
+@app.route('/test/<name>') # passando parametros na URL
+def teste(name=None): # mesmo nome do parametro
+    if name:
+        return 'Olá, %s!' % name
+    else: 
+        return 'Olá, Usuário!'
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
